6c9930e061104b63075e474638d314020815b0eb
kishorekarunakarank@gmail.com <kishorekarunakarank@gmail>
2016-04-18 17:46:54 +0530

    Starting with Simple Examples.

--- Chapter Reactive Streams/src/main/scala/akka/stream/experiments/Example1.scala
+++ Chapter Reactive Streams/src/main/scala/akka/stream/experiments/Example1.scala
@@ -6,20 +6,39 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.util.ByteString
 import java.io.File
+import scala.concurrent.Future
+import akka.Done
+import scala.concurrent.duration.FiniteDuration._
+import scala.concurrent.duration.FiniteDuration
+import java.util.concurrent.TimeUnit
+import java.io.PrintStream
 
-object Example1 extends App{
+object Example1 extends App{  
+  
+  System.setOut(new PrintStream("Result.txt"))
   
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
   
-  val source: Source[Int, NotUsed] = Source(1 to 100)
+  val source: Source[Int, NotUsed] = Source(1 to 1000000)
   source.runForeach { println }(materializer)
   
   val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
   
-  val result = factorials.map {  num  => ByteString(num) }
+  val result: Future[IOResult] = factorials.map {  num  => ByteString(num+"\n") }
                          .runWith(FileIO.toFile(new File("Output.txt")))
-                         
   
-  system.shutdown()
+  // A Reusable lineSink Method                         
+  def lineSink(fileName: String): Sink[String, Future[IOResult]] = {
+    Flow[String].map { num => ByteString(s"$num\n") }
+                .toMat(FileIO.toFile(new File(fileName)))(Keep.right)
+  }
+  factorials.map { _.toString()}.runWith(lineSink("Factorial.txt"))
+  
+  //Time Based Stream Processing
+  val done: Future[Done] = factorials.zipWith(Source(0 to 1000000))((acc, next) => s"$acc != $next")
+                                     .throttle(100, FiniteDuration.apply(5, TimeUnit.SECONDS), 20, ThrottleMode.shaping)
+                                     .runForeach { println }
+                             
+  //system.shutdown()                       
 }
