7fcd292d90f364e6f4395d844ffc15b8cbef8d9f
K Kishore <kishorekarunakarank@gmail.com>
2016-06-20 14:37:03 +0530

    Updated

--- ReactiveAkka/src/main/scala/com/reactive/lesson1/messages/PizzaException.scala
+++
@@ -1,5 +0,0 @@
-package com.reactive.lesson1.messages
-
-final case object PizzaException extends Exception
-final case class ToppingsException(message: String) extends Exception
-final case class PastaException(message: String) extends Exception
